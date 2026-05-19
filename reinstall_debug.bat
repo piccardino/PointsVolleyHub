ed0b34dd0d56f4596e0efa3baf4170c0ae05f09c
piccardino <zeuspiccardinoo@gmail.com>
2026-05-19 10:30:21 +0200

    #

--- reinstall_debug.bat
+++ reinstall_debug.bat
@@ -7,7 +7,7 @@ set "APK_PATH=%SCRIPT_DIR%app\build\outputs\apk\debug\app-debug.apk"
 set "GRADLEW=%SCRIPT_DIR%gradlew.bat"
 set "ADB="
 set "DEVICE_FLAG="
-set "DEFAULT_DEVICE=192.168.1.234:37303"
+set "DEFAULT_DEVICE=10.211.83.249:42303"
 
 if not "%~1"=="" (
     set "DEVICE_FLAG=-s %~1"
